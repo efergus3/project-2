d7a646ac44433c1798accc8e7081c630e2dd224a
Elise Ferguson <efergus3@uoregon.edu>
2020-04-24 17:19:09 -0600

    project 2

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,47 +1,55 @@
 // jshint esversion: 6
 
+let revWord = (word) => {
+  return word.split('').reverse().join('');
+};
+
 //3a. accepts a sentence and returns the string in reverse
 let revWords1 = (str) => {
-  let arr = str.split(" ");
-  let result = "";
+  let arr = str.split(' ');
+  let result = '';
   for (var i = 0; i < arr.length; i++) {
-    result += arr[i];
+    let currentWord = arr[i];
+    let reversedWord = revWord(currentWord);
+    result = result + ' ' + reversedWord;
   }
   return result;
 };
 
 //3b. rewrite with for each
 let revWords2 = (str) => {
-  let arr = str.split("");
-  let result = "";
+  let arr = str.split(' ');
+  let result = '';
   arr.forEach((char)=> {
-    result = char + result;
+    let reversedWord = revWord(char);
+    result = result + ' ' + reversedWord;
   });
   return result;
 };
 
 //3c. rewrite with for in
 let revWords3 = (str) => {
-  let arr = str.split("");
-  let result = "";
+  let arr = str.split(' ');
+  let result = '';
   for (char of arr){
-    result = char + result;
+    let reversedWord = revWord(char);
+    result = result + ' ' + reversedWord;
   }
   return result;
 };
 
-
+/*
 let containsDuplicates = s => {
-  let cArr = [0, 0, 0, 0],
-    code;
-  for (let i in cArr) {
-    code = s[i].toUpperCase().charCodeAt(0) - 65;
+  let charCount = new Array(3).fill(0);
+  for (let i in charCount) {
+    let code = s[i].toUpperCase().charCodeAt(0) - 65;
     //console.log(code);
-    if (cArr[code] > 0) {
+    if (charCount[code] > 0) {
       return true;
     } else {
-      cArr[i]++;
+      charCount[i]++;
     }
   }
   return false;
 };
+*/
